582508db6c37677d06b09f5bd31a771b0787ba10
Cathal O’Riordan <cathal@madein75.com>
2018-07-11 11:06:58 +0100

    Capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for Capistrano 3.6
+lock '3.6.1'
+
+set :application, 'moodle-wit'
+set :repo_url, 'git@github.com:wit-ctel/moodle-34.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+set :scm, :git
+# set :git_strategy, SubmoduleStrategy
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+set :user, 'moodle'
+
+# Default value for :linked_files is []
+#set :linked_files, %w{.htaccess}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
+
+namespace :deploy do
+  before  :starting,    'moodle:enable-maintenance' 
+  after   :published,   'moodle:copy-config'
+  after   :finishing,   'deploy:cleanup'
+  after   :finished,    'php_fpm:restart' 
+  after   :finished,    'moodle:disable-maintenance'
+  after   :finished,    'moodle:purge-caches'
+end
