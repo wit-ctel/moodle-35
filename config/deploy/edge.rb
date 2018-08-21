e990f8359ee19d955dc37203f0e1dd2f4ad989db
Cathal O’Riordan <cathal@madein75.com>
2018-08-21 10:55:46 +0100

    correct server cluster for deploy on Edge

--- config/deploy/edge.rb
+++ config/deploy/edge.rb
@@ -3,6 +3,6 @@ set :deploy_to, "/var/www/edge.moodle.wit.ie/"
 set :branch, ENV["BRANCH_NAME"] || "master"
 
 server 'wit-mdl-web1-1718.hst.heanet.ie', user: 'moodle', roles: %w{ web app admin }
-#server 'wit-moodle-web2.heanet.ie', user: 'moodle', roles: %w{ web }
-#server 'wit-moodle-web3.heanet.ie', user: 'moodle', roles: %w{ web }
-#server 'wit-moodle-web4.heanet.ie', user: 'moodle', roles: %w{ web }
+server 'wit-moodle-web2.heanet.ie', user: 'moodle', roles: %w{ web }
+server 'wit-moodle-web3.heanet.ie', user: 'moodle', roles: %w{ web }
+server 'wit-moodle-web4.heanet.ie', user: 'moodle', roles: %w{ web }
