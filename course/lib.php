814f81dae6af22b615004397929381fbac6eaa49
Cathal O’Riordan <cathal@madein75.com>
2018-09-07 14:59:55 +0100

    add 2 week window to start date of future courses

--- course/lib.php
+++ course/lib.php
@@ -4121,7 +4121,7 @@ function course_classify_for_timeline($course, $user = null, $completioninfo = n
     }
 
     // Start date not reached.
-    if (!empty($course->startdate) && $course->startdate > $today) {
+    if (!empty($course->startdate) && strtotime('-2 weeks', $course->startdate) > $today) {
         return COURSE_TIMELINE_FUTURE;
     }
 
